208e0774baafc1fe03782e775061bc740ce7847a
David A. Kennedy <me@davidakennedy.com>
2021-04-01 13:24:02 -0400

    Add dev dependencies

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@
 const { DateTime } = require("luxon");
 const fs = require("fs");
 const markdownIt = require("markdown-it");
+const markdownItAnchor = require("markdown-it-anchor");
 
 // Configuration and plugins.
 module.exports = function (eleventyConfig) {
@@ -80,6 +81,10 @@ module.exports = function (eleventyConfig) {
     breaks: true,
     typographer: true,
     quotes: "“”‘’",
+  }).use(markdownItAnchor, {
+    permalink: true,
+    permalinkClass: "direct-link",
+    permalinkSymbol: "#",
   });
   eleventyConfig.setLibrary("md", markdownLibrary);
 
