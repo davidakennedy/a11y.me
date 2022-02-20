9815f9b0822383c7b96fc9cb6cb894d5fc42fa7f
David A. Kennedy <me@davidakennedy.com>
2022-02-20 15:24:57 -0500

    Remove unneeded terser code but keep package

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@
 const { DateTime } = require("luxon");
 const fs = require("fs");
 const markdownIt = require("markdown-it");
+const Terser = require("terser");
 
 // Configuration and plugins.
 module.exports = function (eleventyConfig) {
@@ -38,17 +39,6 @@ module.exports = function (eleventyConfig) {
   const CleanCSS = require("clean-css");
   eleventyConfig.addFilter("cssmin", function (code) {
     return new CleanCSS({ sourceMap: true }).minify(code).styles;
-  });
-
-  const Terser = require("terser");
-  eleventyConfig.addFilter("jsmin", function (code) {
-    let minified = Terser.minify(code);
-    if (minified.error) {
-      console.log("Terser error: ", minified.error);
-      return code;
-    }
-
-    return minified.code;
   });
 
   const htmlmin = require("html-minifier");
