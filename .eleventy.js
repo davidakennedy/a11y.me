fd33154ae79bf4cb10a3276d55c4e87163c5cd32
David A. Kennedy <me@davidakennedy.com>
2022-12-04 19:35:04 -0500

    Bring back inline styles for speed

--- .eleventy.js
+++ .eleventy.js
@@ -38,33 +38,9 @@ module.exports = function (eleventyConfig) {
   });
 
   // Development filters
-  // Simple cache busting
-  // https://rob.cogit8.org/posts/2020-10-28-simple-11ty-cache-busting/
-  eleventyConfig.addFilter("bust", (url) => {
-    const [urlPart, paramPart] = url.split("?");
-    const params = new URLSearchParams(paramPart || "");
-    params.set("v", DateTime.local().toFormat("X"));
-    return `${urlPart}?${params}`;
-  });
-
   // Minify CSS
-  eleventyConfig.addTemplateFormats("css");
-
-  eleventyConfig.addExtension("css", {
-    outputFileExtension: "css",
-    compile: async (inputContent) => {
-      return async () => {
-        if (process.env.ELEVENTY_ENV === "prod") {
-          return new Promise((resolve) => {
-            new CleanCSS({}).minify(inputContent, (_, data) => {
-              resolve(data.styles);
-            });
-          });
-        } else {
-          return inputContent.css;
-        }
-      };
-    },
+  eleventyConfig.addFilter("cssmin", function (code) {
+    return new CleanCSS({ sourceMap: true }).minify(code).styles;
   });
 
   eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
